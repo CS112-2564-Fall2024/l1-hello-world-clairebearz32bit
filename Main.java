528b6a85fb95c931fe1281c9e050be785492a6fc
Claire <55174253+clairebearz32bit@users.noreply.github.com>
2024-08-22 00:25:02 +0000

    Added Song Class

--- Main.java
+++ Main.java
@@ -2,4 +2,19 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
     }
+
+    private static String[] createAlbum() {
+        Song[] songs = new Song[3];
+        String[] names, artists;
+        double[] lengths;
+
+        for(int i = 0 i < 3; i++) {
+            
+        }
+    }
+
+    private static void printAlbum() {
+
+    }
+
 }
