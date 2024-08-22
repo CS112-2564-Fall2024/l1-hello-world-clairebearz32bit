de107f84b674dcdd4bf271ee8334677b86aeffe1
Claire <55174253+clairebearz32bit@users.noreply.github.com>
2024-08-22 00:35:26 +0000

    Fixed Song not working

--- Main.java
+++ Main.java
@@ -1,17 +1,22 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
-    }
-
-    private static String[] createAlbum() {
         Song[] songs = new Song[3];
-        String[] names, artists;
-        double[] lengths;
-
-        for(int i = 0 i < 3; i++) {
-            
-        }
+        System.out.println(songs[0].getName());
     }
+
+    // private static String[] createAlbum() {
+    // System.out.println(new Song().getName());
+    // Song[] songs = new Song[3];
+    // String[] names, artists;
+    // // double[] lengths;
+    // // // names = {"Toxic", "Diamonds", "Mask Off"};
+    // // // artists = {"Britney Spears", "Rihanna", "Future"};
+        
+    // for (int i = 0; i < 3; i++) {
+            
+    // }
+    // }
 
     private static void printAlbum() {
 
